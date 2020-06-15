72817b9e8b7f24d7825292a317a81f7834cebf2e
BitterBlue22 <telisax@gmail.com>
2020-06-15 11:45:45 +0100

    adds server-side firebase authentication setup

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-const express = require('express');
-const graphQlHttp = require('express-graphql');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const graphQlHttp = require("express-graphql");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const admin = require("firebase-admin");
 
 // const graphQlSchema = require('./graphql/schema/index');
 // const graphQlResolvers = require('./graphql/resolvers/index');
 
-const schema = require('./graphql/schema/schema')
-
+const schema = require("./graphql/schema/schema");
 
 const app = express();
 
 app.use(cors());
 
-app.use('/graphql', graphQlHttp({
-  schema,
-  graphiql: true
-}))
+app.use(
+  "/graphql",
+  graphQlHttp({
+    schema,
+    graphiql: true,
+  })
+);
 
-app.use('/', (req, res, send) => {
-  res.send('Chicken Tinder')
-})
+app.use("/", (req, res, send) => {
+  res.send("Chicken Tinder");
+});
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`App is live on Port ${process.env.PORT}`)
-})
+  console.log(`App is live on Port ${process.env.PORT}`);
+});
 
-mongoose.connect(process.env.MONGODB_URI, { useFindAndModify: false })
-  .catch(err => {
+mongoose
+  .connect(process.env.MONGODB_URI, { useFindAndModify: false })
+  .catch((err) => {
     console.log(err);
-  })
-
+  });
